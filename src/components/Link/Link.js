6b97947a8fdf09d74a57a340ea631a8dbaf03d7d
Dylan Frankland <github@frankland.io>
2015-12-14 19:55:08 -0800

    Fix for Strict Usage of `this`
    Babel 6 sets the top level `this` to be `undefined`. To get around this
    fat arrow functions should only be bound when the static function is
    executed.

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -21,40 +21,42 @@ class Link extends Component {
     onClick: PropTypes.func,
   };
 
-  static handleClick = (event) => {
-    let allowTransition = true;
-    let clickResult;
+  static handleClick(event) {
+    return (event) => {
+      let allowTransition = true;
+      let clickResult;
 
-    if (this.props && this.props.onClick) {
-      clickResult = this.props.onClick(event);
-    }
-
-    if (isModifiedEvent(event) || !isLeftClickEvent(event)) {
-      return;
-    }
-
-    if (clickResult === false || event.defaultPrevented === true) {
-      allowTransition = false;
-    }
-
-    event.preventDefault();
-
-    if (allowTransition) {
-      const link = event.currentTarget;
-      if (this.props && this.props.to) {
-        Location.push({
-          ...(parsePath(this.props.to)),
-          state: this.props && this.props.state || null,
-        });
-      } else {
-        Location.push({
-          pathname: link.pathname,
-          search: link.search,
-          state: this.props && this.props.state || null,
-        });
+      if (this.props && this.props.onClick) {
+        clickResult = this.props.onClick(event);
       }
-    }
-  };
+
+      if (isModifiedEvent(event) || !isLeftClickEvent(event)) {
+        return;
+      }
+
+      if (clickResult === false || event.defaultPrevented === true) {
+        allowTransition = false;
+      }
+
+      event.preventDefault();
+
+      if (allowTransition) {
+        const link = event.currentTarget;
+        if (this.props && this.props.to) {
+          Location.push({
+            ...(parsePath(this.props.to)),
+            state: this.props && this.props.state || null,
+          });
+        } else {
+          Location.push({
+            pathname: link.pathname,
+            search: link.search,
+            state: this.props && this.props.state || null,
+          });
+        }
+      }
+    };
+  }
 
   render() {
     const { to, query, ...props } = this.props;
